bc2b9b608d19164a6b888aedb1b0bb84a9f91792
mahdi-salah <mustafa_alsaidy@ymail.com>
2021-02-16 14:11:53 +0300

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::group(['middleware' => ['auth']], function() {
+    // your routes
     Route::get('/', function () {
         return view('welcome');
     });
