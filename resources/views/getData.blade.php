bedf08747eb86cda3215c6787e43112c20214be5
mahdi-salah <mustafa_alsaidy@ymail.com>
2021-04-26 16:42:24 +0300

    initial commit

--- resources/views/getData.blade.php
+++ resources/views/getData.blade.php
@@ -31,67 +31,6 @@ $d_4049 = $points['d_4049'];
 ?>
 
 
-{{--<div id="refresh">--}}
-{{--    <section class="mt-4 container-fluid px-5 px-sm-0 w-sm-75 w-md-75 w-lg-50">--}}
-{{--        <!--start table-->--}}
-{{--        <table class="table shadow-lg table-hover table-sm table-dark text-center rounded-lg overflow-hidden">--}}
-{{--            <thead>--}}
-{{--            <tr class="myBgColor">--}}
-{{--                <th colspan="2" class="pt-2 pb-0"><h4>Gen. Units</h4></th>--}}
-{{--            </tr>--}}
-{{--            </thead>--}}
-{{--            <tbody>--}}
-{{--            <tr>--}}
-{{--                <td><h6>GTG1</h6></td>--}}
-{{--                <td><h6><?php echo $d_4041 ?></h6></td>--}}
-{{--            </tr>--}}
-{{--            <tr>--}}
-{{--                <td><h6>GTG2</h6></td>--}}
-{{--                <td><h6><?php echo $d_4042 ?></h6></td>--}}
-{{--            </tr>--}}
-{{--            <tr>--}}
-{{--                <td><h6>GTG3</h6></td>--}}
-{{--                <td><h6><?php echo $d_4043 ?></h6></td>--}}
-{{--            </tr>--}}
-{{--            <tr>--}}
-{{--                <td><h6>GTG4</h6></td>--}}
-{{--                <td><h6><?php echo $d_4044 ?></h6></td>--}}
-{{--            </tr>--}}
-{{--            <tr>--}}
-{{--                <td><h6>GTG5</h6></td>--}}
-{{--                <td><h6><?php echo $d_4045 ?></h6></td>--}}
-{{--            </tr>--}}
-{{--            <tr>--}}
-{{--                <td><h6>GTG6</h6></td>--}}
-{{--                <td><h6><?php echo $d_4046 ?></h6></td>--}}
-{{--            </tr>--}}
-{{--            <tr>--}}
-{{--                <td><h6>STG7</h6></td>--}}
-{{--                <td><h6><?php echo $d_4047 ?></h6></td>--}}
-{{--            </tr>--}}
-{{--            <tr>--}}
-{{--                <td><h6>STG8</h6></td>--}}
-{{--                <td><h6><?php echo $d_4048 ?></h6></td>--}}
-{{--            </tr>--}}
-{{--            <tr>--}}
-{{--                <td><h6>STG9</h6></td>--}}
-{{--                <td><h6><?php echo $d_4049 ?></h6></td>--}}
-{{--            </tr>--}}
-{{--            </tbody>--}}
-{{--            <tfoot>--}}
-{{--            <tr class="myBgColor">--}}
-{{--                <td class="text-right pr-5 pt-2 pb-0"><h4>Total</h4></td>--}}
-{{--                <td class="text-left pl-5 pt-2 pb-0"><h4><?php echo $d_4041 + $d_4042 + $d_4043 + $d_4044 + $d_4045 + $d_4046 + $d_4047 + $d_4048 + $d_4049 ?></h4></td>--}}
-{{--            </tr>--}}
-{{--            </tfoot>--}}
-{{--        </table>--}}
-{{--        <!--end table-->--}}
-{{--    </section>--}}
-
-{{--</div>--}}
-
-
-
 <div id="refresh">
     <section class="mt-4 container-fluid px-5 px-sm-0 w-sm-75 w-md-75 w-lg-50">
         <!--start table-->
@@ -104,39 +43,39 @@ $d_4049 = $points['d_4049'];
             <tbody>
             <tr>
                 <td><h6>GTG1</h6></td>
-                <td><h6>145.1</h6></td>
+                <td><h6><?php echo $d_4041 ?></h6></td>
             </tr>
             <tr>
                 <td><h6>GTG2</h6></td>
-                <td><h6>144</h6></td>
+                <td><h6><?php echo $d_4042 ?></h6></td>
             </tr>
             <tr>
                 <td><h6>GTG3</h6></td>
-                <td><h6>145</h6></td>
+                <td><h6><?php echo $d_4043 ?></h6></td>
             </tr>
             <tr>
                 <td><h6>GTG4</h6></td>
-                <td><h6>146</h6></td>
+                <td><h6><?php echo $d_4044 ?></h6></td>
             </tr>
             <tr>
                 <td><h6>GTG5</h6></td>
-                <td><h6>148</h6></td>
+                <td><h6><?php echo $d_4045 ?></h6></td>
             </tr>
             <tr>
                 <td><h6>GTG6</h6></td>
-                <td><h6>150</h6></td>
+                <td><h6><?php echo $d_4046 ?></h6></td>
             </tr>
             <tr>
                 <td><h6>STG7</h6></td>
-                <td><h6>0</h6></td>
+                <td><h6><?php echo $d_4047 ?></h6></td>
             </tr>
             <tr>
                 <td><h6>STG8</h6></td>
-                <td><h6>0</h6></td>
+                <td><h6><?php echo $d_4048 ?></h6></td>
             </tr>
             <tr>
                 <td><h6>STG9</h6></td>
-                <td><h6>0</h6></td>
+                <td><h6><?php echo $d_4049 ?></h6></td>
             </tr>
             </tbody>
             <tfoot>
@@ -150,6 +89,67 @@ $d_4049 = $points['d_4049'];
     </section>
 
 </div>
+
+
+
+{{--<div id="refresh">--}}
+{{--    <section class="mt-4 container-fluid px-5 px-sm-0 w-sm-75 w-md-75 w-lg-50">--}}
+{{--        <!--start table-->--}}
+{{--        <table class="table shadow-lg table-hover table-sm table-dark text-center rounded-lg overflow-hidden">--}}
+{{--            <thead>--}}
+{{--            <tr class="myBgColor">--}}
+{{--                <th colspan="2" class="pt-2 pb-0"><h4>Gen. Units</h4></th>--}}
+{{--            </tr>--}}
+{{--            </thead>--}}
+{{--            <tbody>--}}
+{{--            <tr>--}}
+{{--                <td><h6>GTG1</h6></td>--}}
+{{--                <td><h6>145.1</h6></td>--}}
+{{--            </tr>--}}
+{{--            <tr>--}}
+{{--                <td><h6>GTG2</h6></td>--}}
+{{--                <td><h6>144</h6></td>--}}
+{{--            </tr>--}}
+{{--            <tr>--}}
+{{--                <td><h6>GTG3</h6></td>--}}
+{{--                <td><h6>145</h6></td>--}}
+{{--            </tr>--}}
+{{--            <tr>--}}
+{{--                <td><h6>GTG4</h6></td>--}}
+{{--                <td><h6>146</h6></td>--}}
+{{--            </tr>--}}
+{{--            <tr>--}}
+{{--                <td><h6>GTG5</h6></td>--}}
+{{--                <td><h6>148</h6></td>--}}
+{{--            </tr>--}}
+{{--            <tr>--}}
+{{--                <td><h6>GTG6</h6></td>--}}
+{{--                <td><h6>150</h6></td>--}}
+{{--            </tr>--}}
+{{--            <tr>--}}
+{{--                <td><h6>STG7</h6></td>--}}
+{{--                <td><h6>0</h6></td>--}}
+{{--            </tr>--}}
+{{--            <tr>--}}
+{{--                <td><h6>STG8</h6></td>--}}
+{{--                <td><h6>0</h6></td>--}}
+{{--            </tr>--}}
+{{--            <tr>--}}
+{{--                <td><h6>STG9</h6></td>--}}
+{{--                <td><h6>0</h6></td>--}}
+{{--            </tr>--}}
+{{--            </tbody>--}}
+{{--            <tfoot>--}}
+{{--            <tr class="myBgColor">--}}
+{{--                <td class="text-right pr-5 pt-2 pb-0"><h4>Total</h4></td>--}}
+{{--                <td class="text-left pl-5 pt-2 pb-0"><h4><?php echo $d_4041 + $d_4042 + $d_4043 + $d_4044 + $d_4045 + $d_4046 + $d_4047 + $d_4048 + $d_4049 ?></h4></td>--}}
+{{--            </tr>--}}
+{{--            </tfoot>--}}
+{{--        </table>--}}
+{{--        <!--end table-->--}}
+{{--    </section>--}}
+
+{{--</div>--}}
 
 
 
